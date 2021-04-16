c87b8430c56cd35a36482dc3a69e916621532633
Liz McManus <liz.mcmanus93@gmail.com>
2021-04-16 16:30:40 +0100

    new tables added and figure fixed

--- stats_compare.py
+++ stats_compare.py
@@ -24,7 +24,7 @@ def stats_compare(data, receptors, subunits, mask_names,n_samples=10000):
 
     #creating blank figure for comparison plots
     out_pdf = pdf.PdfPages("regional_comparison.pdf");
-    fig,axs = plt.subplots(nrows=3, ncols=2,sharex=False,sharey=False, figsize=(35, 44))
+    fig,axs = plt.subplots(nrows=3, ncols=2,sharex=False,sharey=False, figsize=(44, 50))
     plt.tick_params(bottom=False, top=False, left=False, right=False)
     fig.text(0.08, 0.5, 'Normalised mRNA Expression Value', va='center', ha='center', rotation='vertical', fontsize=30)
     a=0
@@ -47,10 +47,11 @@ def stats_compare(data, receptors, subunits, mask_names,n_samples=10000):
         alpha = 0.05/len(r_sub)
         #loop subunits
         for x, sub in enumerate(r_sub[0]):
-            print(f'{r} - {sub}')
+           # print(f'{r} - {sub}')
             # Get data for each mask
             mask1_data = data[mask_names[0]][r][:,x]
             mask2_data = data[mask_names[1]][r][:,x]
+            
 
             # Remove zeros
             # Do we want to do this though? The zeros skew the distribution so
@@ -64,26 +65,45 @@ def stats_compare(data, receptors, subunits, mask_names,n_samples=10000):
 
             dvals.append(D)
             dcis_up.append(Dci_up)
-            dcis_low.append(dcis_low)
+            dcis_low.append(Dci_low)
             pctDifs.append(pctDif)
-
+           
+            ##### added this back in so the data is used for the violins 
+            for z, m in enumerate(mask_names):
+                #restructuring data to compare subunits between masks
+                data_col = pd.DataFrame(data[m][r][:,x])
+                data_col.columns = ['value']
+                data_col['mask'] = m
+                #subunit data for one mask
+                data_col['subunit'] = sub
+                #subunit data for all masks
+                compare_data = pd.concat([compare_data,data_col])
+        
 
         #corrections for multiple comparisons
         # pvals_numpy = np.array(pvals).T
         # corrected_pvals = multi.multipletests(pvals_numpy, alpha=0.05, method='bonferroni')
         # orig_pvals = pd.DataFrame(pvals).T
         # new_pvals = pd.DataFrame(corrected_pvals[1]).T
-        dvals = pd.DataFrame(dvals).T
-        subunit_outputs = pd.concat([pctDifs, dvals, dcis_low, dcis_up])
+        dvals = pd.DataFrame(dvals)
+        pctDifs_df = pd.DataFrame(pctDifs)
+        dcis_low_df = pd.DataFrame(dcis_low)
+        dcis_up_df = pd.DataFrame(dcis_up)
+        
+        print("Concatinating Data")
+        subunit_outputs = pd.concat([pctDifs_df, dvals, dcis_low_df, dcis_up_df], axis=1)
+        subunit_outputs = subunit_outputs.T
         subunit_outputs.columns = [r_sub[0]]
-        subunit_outputs.index = ['pct','d', 'dCI_low','dCI_up']
+       # subunit_outputs.rows = ['pct','d', 'dCI_low','dCI_up']  #### still an issue here
 
         alldata_reorder[r]=compare_data
         alldata_outputs[r]=subunit_outputs
 
         #remove 0's and make violin plots
+        print("Removing 0's")
         df_removed = compare_data[(compare_data != 0).all(1)]
 
+        print("Generating Violin Plots")
         sns.set_palette("hls", 8)
         plot = sns.violinplot(x="subunit", y="value", hue="mask", data=df_removed, ax=axs[b,a])
         plot.set_title(r, fontsize=26)
@@ -101,22 +121,23 @@ def stats_compare(data, receptors, subunits, mask_names,n_samples=10000):
         cell_text = []
         tmp = alldata_outputs[r].to_numpy()
 
-        cell_text[[f'{}']]
+        #cell_text[[f'{r}']] ###WTF does this do?!
 
-        # for row in range(0, len(tmp)):
-        #     cell_text.append(['%1.4f' % (value) for value in tmp[row,:]])
-        rows = ['% difference','d', 'd 95% CI']
+        for row in range(0, len(tmp)):
+            cell_text.append(['%1.2f' % (value) for value in tmp[row,:]])
+        rows = ['% difference','d', 'd 95% CI lower', 'd 95% CI upper']
         columns = ['%s' % (unit) for unit in r_sub[0]]
         the_table = plot.table(cellText=cell_text,
-                  rowLabels=rows,
-                  colLabels=columns,
-                  loc='bottom')
+              rowLabels=rows,
+              colLabels=columns,
+              loc='bottom')
         the_table.set_fontsize(16)
         the_table.scale(1,2)
 
-        a =+ 1
+
+        a += 1 
         if a > 1:
-            b =+ 1
+            b += 1
             a = 0
 
     out_pdf.savefig()
