6673013f62e51fd4189feffa70ac5ac6ab26bced
nwd2918 <nwduncan@tmu.edu.tw>
2022-08-25 13:13:09 +0800

    Features in main script to asign basic values and create output directory

--- inspectro_gadget/inspectro_gadget.py
+++ inspectro_gadget/inspectro_gadget.py
@@ -10,3 +10,43 @@ GABA and Glutamate regions must ine in a .tsv file "receptors.tsv"
 
 """
 
+import os
+import time
+from inspectro_gadget.io import GadgetData, is_valid
+
+
+def inspectro_gadget(mask_images, mask_labels=None, in_dir=None, out_root=None, create_overlap=False):
+
+    # Check mask images are entered as a list
+    mask_images = is_valid(mask_images, list)
+    # Count number of regions
+    if sum(isinstance(i, list) for i in mask_images) == 0:
+        no_regions = 1
+    else:
+        no_regions = sum(isinstance(i, list) for i in mask_images)
+
+    # Test if there are multiple subjects
+    multi_sub = False
+    if no_regions == 1:
+        if len(mask_images) > 1:
+            multi_sub = True
+            no_subjects = len(mask_images)
+
+    # Create mask labels if not given
+    if not mask_labels:
+        mask_labels = []
+        for mask_no in no_regions:
+            mask_labels.append(f'Region {mask_no}')
+
+    # Create output directory
+    if out_root:
+        if not os.path.isdir(out_root):
+            raise IsADirectoryError('The directory to create the output folder in does not exist.')
+        out_dir = os.path.join(out_root, f'gadget-out_{time.strftime("%Y%m%d-%H%M%S")}')
+    else:
+        out_dir = os.path.join(os.getcwd(), f'gadget-out_{time.strftime("%Y%m%d-%H%M%S")}')
+    if not os.path.isdir(out_dir):
+        os.path.mkdir(out_dir)
+    else:
+        raise IsADirectoryError('Output directory already exists.')
+
