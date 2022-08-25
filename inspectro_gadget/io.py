935dda3a7a58629ec3a3a5de8e2d7526460256ce
nwd2918 <nwduncan@tmu.edu.tw>
2022-08-25 16:53:17 +0800

    Prepared data class for getting all inputs

--- inspectro_gadget/io.py
+++ inspectro_gadget/io.py
@@ -4,7 +4,11 @@
 Resources for handling data input, output, and organisation.
 """
 
+import os
 import nibabel as ni
+import numpy as np
+import pandas as pd
+import inspect
 from copy import deepcopy
 
 def is_valid(var, var_type, list_type=None):
@@ -37,6 +41,7 @@ def is_valid(var, var_type, list_type=None):
             _ = is_valid(element, list_type)
 
     return var
+
 
 def test_nifti_ext(fname):
     """
@@ -86,12 +91,76 @@ def load_nifti(fname):
     return img.get_fdata()
 
 
+def extract_mrna(subunit_path, region_mask):
+    """
+    Extract the mRNA expression values within a mask region and normalise relative to cortical expression through
+    robust sigmoid method.
+
+    Parameters
+    ----------
+    subunit_path: string
+        Path to image file containing mRNA values
+    region_mask: array
+        Numpy array containing voxel mask
+
+    Returns
+    -------
+    region_norm:
+        Normalised expression values for all voxels within mask
+
+    """
+    mrna = ni.load(subunit_path).get_fdata()
+    mrna_removed = np.array(mrna[(mrna != 0)])
+
+    # interquartile range
+    median = np.median(mrna_removed)
+    Q1 = np.percentile(mrna_removed, 25)
+    Q3 = np.percentile(mrna_removed, 75)
+    IQRx = (Q3 - Q1) / 1.35
+
+    # robust sigmoid
+    image_norm = 1 / (1 + np.exp(-(mrna - median) / IQRx))
+    region_norm = image_norm[region_mask == 1]
+
+    return region_norm
+
+def get_receptor_data(receptors, mask, data_dir):
+    """
+    Extract mRNA data for each gene from a given mask.
+
+    mRNA expression is normalised to cortical values.
+
+    Parameters
+    ----------
+    receptors: list
+        List of gene names (corresponding to the names used in the image file names)
+    mask: array
+        Binary mask indicating the MRS voxel
+    data_dir: string
+        Directory where module data is located.
+
+    Returns
+    -------
+    Numpy array with a column for each gene. Each row is a voxel.
+
+    """
+    out = np.zeros((len(mask[mask==1]), len(receptors)))
+    for rr, receptor in enumerate(receptors):
+        out[:, rr] = extract_mrna(os.path.join(data_dir, 'mRNA_images', f'{receptor}_mirr_mRNA.nii'), mask)
+    return out
 
 
-class GadgetData():
+class GadgetData:
     """
     Attributes
     ----------
+                mask_data: list
+                A list comtaining numpy arrays of the MRS masks to work with
+                If a single mask then a list with a single 3d array;
+                if multiple masks a list of multiple 3d;
+                if subject masks a list of many strings
+            labels: list
+                Labels for the masks
 
     Methods
     -------
@@ -100,25 +169,43 @@ class GadgetData():
     -----
     """
 
-    def __init__(self, mask_data, labels, mrnas, no_regions, no_subjects):
+    def __init__(self, mask_fnames, labels, no_regions, multi_subject=False, no_subjects=1):
         """
         Initialise data object.
-
-            Parameters
-            ----------
-            mask_data: list
-                A list comtaining numpy arrays of the MRS masks to work with
-                If a single mask then a list with a single 3d array;
-                if multiple masks a list of multiple 3d;
-                if subject masks a list of many strings
-            labels: list
-                Labels for the masks
-
-
         """
-        self.mask_data = deepcopy(mask_data)
+        #  Inititate user-defined parameters
+        self.mask_fnames = deepcopy(mask_fnames)
         self.labels = deepcopy(labels)
-        self.mrnas = None
         self.no_regions = deepcopy(no_regions)
         self.no_subjects = deepcopy((no_subjects))
+        self.multi_subject = deepcopy(multi_subject)
+        if self.no_regions > 1:
+            self.multi_region = True
+        else:
+            self.multi_region = False
+
+        # Load built-in data
+        data_dir = '/media/storage2/Inspectro-Gadget/inspectro_gadget/data' ###### Change this!!!
+        #data_dir = os.path.dirname(inspect.getfile(inspectro_gadget))
+        self.receptor_list = pd.read_csv(os.path.join(data_dir, 'GroupedReceptors.tsv'), delimiter='\t', header=None)
+        tmp_img = ni.load(os.path.join(data_dir, 'MNI152_T1_2mm.nii.gz'))
+        self.img_affine = tmp_img.affine
+        self.bground_image = tmp_img.get_fdata()
+
+        # Load mask(s) and receptor data
+        self.mask_images = {}
+        self.receptor_data = {}
+        if self.multi_subject:
+            self.mask_images[self.labels[0]] = []
+            self.receptor_data[self.labels[0]] = []
+            for ss in range(no_subjects):
+                self.mask_images[self.labels[0]].append(load_nifti(self.mask_fnames[ss]))
+                self.receptor_data[labels[0]].append(get_receptor_data(self.receptor_list.iloc[:, 0].values,
+                                                              self.mask_images[labels[0]][ss], data_dir))
+        else:
+            for ll, label in enumerate(labels):
+                self.mask_images[label] = load_nifti(self.mask_fnames[ll])
+                self.receptor_data[label] = get_receptor_data(self.receptor_list.iloc[:, 0].values,
+                                                              self.mask_images[label], data_dir)
+
 
