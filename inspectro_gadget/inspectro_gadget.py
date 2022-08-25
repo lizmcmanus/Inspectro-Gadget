a35a4bcb72b662b789c6cedce03bb173d90c5ac5
nwd2918 <nwduncan@tmu.edu.tw>
2022-08-25 13:41:38 +0800

    Features in main script to create primary data object

--- inspectro_gadget/inspectro_gadget.py
+++ inspectro_gadget/inspectro_gadget.py
@@ -15,22 +15,22 @@ import time
 from inspectro_gadget.io import GadgetData, is_valid
 
 
-def inspectro_gadget(mask_images, mask_labels=None, in_dir=None, out_root=None, create_overlap=False):
+def inspectro_gadget(mask_fnames, mask_labels=None, in_dir=None, out_root=None, create_overlap=False):
 
     # Check mask images are entered as a list
-    mask_images = is_valid(mask_images, list)
+    mask_fnames = is_valid(mask_fnames, list)
     # Count number of regions
-    if sum(isinstance(i, list) for i in mask_images) == 0:
+    if sum(isinstance(i, list) for i in mask_fnames) == 0:
         no_regions = 1
     else:
-        no_regions = sum(isinstance(i, list) for i in mask_images)
+        no_regions = sum(isinstance(i, list) for i in mask_fnames)
 
     # Test if there are multiple subjects
     multi_sub = False
     if no_regions == 1:
-        if len(mask_images) > 1:
+        if len(mask_fnames) > 1:
             multi_sub = True
-            no_subjects = len(mask_images)
+            no_subjects = len(mask_fnames)
 
     # Create mask labels if not given
     if not mask_labels:
@@ -46,7 +46,12 @@ def inspectro_gadget(mask_images, mask_labels=None, in_dir=None, out_root=None, 
     else:
         out_dir = os.path.join(os.getcwd(), f'gadget-out_{time.strftime("%Y%m%d-%H%M%S")}')
     if not os.path.isdir(out_dir):
-        os.path.mkdir(out_dir)
+        os.mkdir(out_dir)
     else:
         raise IsADirectoryError('Output directory already exists.')
 
+    # Collect all required data
+    if multi_sub:
+        data = GadgetData(mask_fnames, mask_labels, no_regions, multi_sub=multi_sub, no_subjects=no_subjects)
+    else:
+        data = GadgetData(mask_fnames, mask_labels, no_regions)
