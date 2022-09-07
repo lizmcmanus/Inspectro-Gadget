db9141011cb82fe6f9952736876bbb9713b281d9
nwd2918 <nwduncan@tmu.edu.tw>
2022-09-07 17:22:38 +0800

    Added function to calculate excitation inhibition balance

--- inspectro_gadget/stats.py
+++ inspectro_gadget/stats.py
@@ -33,7 +33,6 @@ def compare_regions(subunit_data, receptor_list, n_samples=5000):
     Returns
     -------
 
-
     """
     region_names = list(subunit_data.keys())
     subunit_d_vals = {}
@@ -63,6 +62,37 @@ def compare_regions(subunit_data, receptor_list, n_samples=5000):
     return subunit_d_vals, subunit_d_cis, subunit_pct_diff, subunit_ks_vals
 
 
+def ex_in(subunit_data, receptor_list):
+    """
+    Calculate the excitation/inhibition ratio for a region.
+
+    Divides the sum of AMPA+NMDA expression by the sum of GABAA(alpha, beta, gamma) expression.
+
+    Definition taken from Deco et al, Dynamical consequences of regional heterogeneity in the brain’s transcriptional
+    landscape. Science Advances, 7(29):eabf4752, 2021.
+
+    Parameters
+    ----------
+    subunit_data: dict
+        Dictionary with the expression data per subunit
+    receptor_list: dataframe
+        Dataframe containing the list of receptor subunits and the receptors they compose. Analysis is grouped by
+        receptor type
+
+    Returns
+    -------
+    ex_in: float
+        Excitation inhibition ratio
+
+    """
+    gabaa_a = np.nansum(subunit_data[receptor_list.subunit[receptor_list.grouping == 'GABAA_Alpha'].values])
+    gabaa_b = np.nansum(subunit_data[receptor_list.subunit[receptor_list.grouping == 'GABAA_Beta'].values])
+    gabaa_g = np.nansum(subunit_data[receptor_list.subunit[receptor_list.grouping == 'GABAA_Gamma'].values])
+    nmda = np.nansum(subunit_data[receptor_list.subunit[receptor_list.grouping == 'NMDA'].values])
+    ampa = np.nansum(subunit_data[receptor_list.subunit[receptor_list.grouping == 'AMPA'].values])
+    return (nmda+ampa)/(gabaa_a+gabaa_b+gabaa_g)
+
+
 def calc_cohend(g1, g2):
     """
     Calculate Cohen's d for a comparison of two dependent groups.
@@ -74,7 +104,6 @@ def calc_cohend(g1, g2):
     m1 = np.mean(g1)
     m2 = np.mean(g2)
     sd = np.std(g1) + np.std(g2) / 2
-    #sd = np.std(mstats.winsorize(g1,limits=(0.1,0.1)))+np.std(mstats.winsorize(g2,limits=(0.1,0.1)))/2
     return (m1 - m2) / sd
 
 
@@ -176,6 +205,4 @@ def bootstrap_diff(g1, g2, n_samples=10000, alpha=0.05):
     g2_mad = mad_median(g2)
     mDif = np.median(g1_mad)-np.median(g2_mad)
     pctDif = mDif/np.median(g2_mad)*100
-    #mPerm = np.array(Parallel(n_jobs=-2,verbose=0)(delayed(perm_median)(g1_mad,g2_mad)for i in range(n_samples)))
-    # p = len(np.where(np.abs(mPerm)>=np.abs(mDif))[0])/float(n_samples)
     return pctDif, D, Dci
