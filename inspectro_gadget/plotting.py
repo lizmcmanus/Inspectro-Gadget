ec2207479a2bea6b9c9087b0eb0dfb9ce1c06ce4
nwd2918 <nwduncan@tmu.edu.tw>
2023-05-26 20:55:40 +0800

    Updated plotting so that colours match for different regions in the two region plots

--- inspectro_gadget/plotting.py
+++ inspectro_gadget/plotting.py
@@ -10,6 +10,7 @@ import matplotlib.gridspec as gridspec
 import numpy as np
 import pandas as pd
 import seaborn as sb
+from matplotlib import colors
 from matplotlib import table
 from matplotlib.ticker import AutoMinorLocator
 from scipy.ndimage import center_of_mass
@@ -38,37 +39,68 @@ def plot_masks(mask_imgs, labels, bground, pdf, ex_in=None):
     Figure object
 
     """
+
     no_ax = len(labels)
     fig = plt.figure(tight_layout=True)
     gs = gridspec.GridSpec(no_ax, 3, width_ratios=[1, 1, 0.835])
-    for aa in range(no_ax):
-        cmass = np.round(center_of_mass(mask_imgs[labels[aa]]), 0).astype(int)
+    if no_ax == 1:
+        cmass = np.round(center_of_mass(mask_imgs[labels[0]]), 0).astype(int)
         # Sagittal
-        mask_roi = np.ma.masked_where(mask_imgs[labels[aa]][cmass[0], :, :] == 0, mask_imgs[labels[aa]][cmass[0], :, :])
-        ax = fig.add_subplot(gs[aa, 0])
+        mask_roi = np.ma.masked_where(mask_imgs[labels[0]][cmass[0], :, :] == 0, mask_imgs[labels[0]][cmass[0], :, :])
+        ax = fig.add_subplot(gs[0, 0])
         ax.imshow(bground[cmass[0], :, :].T, cmap='Greys_r', origin='lower', interpolation='none')
         ax.imshow(mask_roi.T, origin='lower', interpolation='none', alpha=0.8)
         ax.set(yticklabels=[], xticklabels=[])  # remove the tick labels
         ax.tick_params(left=False, bottom=False)
         # Coronal
-        mask_roi = np.ma.masked_where(mask_imgs[labels[aa]][:, cmass[1], :] == 0, mask_imgs[labels[aa]][:, cmass[1], :])
-        ax = fig.add_subplot(gs[aa, 1])
+        mask_roi = np.ma.masked_where(mask_imgs[labels[0]][:, cmass[1], :] == 0, mask_imgs[labels[0]][:, cmass[1], :])
+        ax = fig.add_subplot(gs[0, 1])
         ax.imshow(bground[:, cmass[1], :].T, cmap='Greys_r', origin='lower', interpolation='none')
         ax.imshow(mask_roi.T, origin='lower', interpolation='none', alpha=0.8)
         ax.set(yticklabels=[], xticklabels=[])  # remove the tick labels
         ax.tick_params(left=False, bottom=False)
         if ex_in:
-            ax.set_title(f'{labels[aa]}\nEstimated excitation/inhibition ratio = {ex_in[labels[aa]]:0.2f}')
+            ax.set_title(f'{labels[0]}\nEstimated excitation/inhibition ratio = {ex_in[labels[0]]:0.2f}')
         else:
-            ax.set_title(labels[aa])
+            ax.set_title(labels[0])
         # Axial
-        mask_roi = np.ma.masked_where(mask_imgs[labels[aa]][:, :, cmass[2]] == 0, mask_imgs[labels[aa]][:, :, cmass[2]])
-        ax = fig.add_subplot(gs[aa, 2])
+        mask_roi = np.ma.masked_where(mask_imgs[labels[0]][:, :, cmass[2]] == 0, mask_imgs[labels[0]][:, :, cmass[2]])
+        ax = fig.add_subplot(gs[0, 2])
         ax.imshow(bground[:, :, cmass[2]].T, cmap='Greys_r', origin='lower', interpolation='none')
         ax.imshow(mask_roi.T, origin='lower', interpolation='none', alpha=0.8)
         ax.set(yticklabels=[], xticklabels=[])  # remove the tick labels
         ax.tick_params(left=False, bottom=False)
-    pdf.savefig(fig)
+        pdf.savefig(fig)
+    else:
+        colours = ["#0086a8", "#0086a8"]
+        for aa in range(no_ax):
+            cmass = np.round(center_of_mass(mask_imgs[labels[aa]]), 0).astype(int)
+            # Sagittal
+            mask_roi = np.ma.masked_where(mask_imgs[labels[aa]][cmass[0], :, :] == 0, mask_imgs[labels[aa]][cmass[0], :, :])
+            ax = fig.add_subplot(gs[aa, 0])
+            ax.imshow(bground[cmass[0], :, :].T, cmap='Greys_r', origin='lower', interpolation='none')
+            ax.imshow(mask_roi.T, origin='lower', interpolation='none', alpha=0.8, cmap=colors.ListedColormap([colours[aa]]))
+            ax.set(yticklabels=[], xticklabels=[])  # remove the tick labels
+            ax.tick_params(left=False, bottom=False)
+            # Coronal
+            mask_roi = np.ma.masked_where(mask_imgs[labels[aa]][:, cmass[1], :] == 0, mask_imgs[labels[aa]][:, cmass[1], :])
+            ax = fig.add_subplot(gs[aa, 1])
+            ax.imshow(bground[:, cmass[1], :].T, cmap='Greys_r', origin='lower', interpolation='none')
+            ax.imshow(mask_roi.T, origin='lower', interpolation='none', alpha=0.8, cmap=colors.ListedColormap([colours[aa]]))
+            ax.set(yticklabels=[], xticklabels=[])  # remove the tick labels
+            ax.tick_params(left=False, bottom=False)
+            if ex_in:
+                ax.set_title(f'{labels[aa]}\nEstimated excitation/inhibition ratio = {ex_in[labels[aa]]:0.2f}')
+            else:
+                ax.set_title(labels[aa])
+            # Axial
+            mask_roi = np.ma.masked_where(mask_imgs[labels[aa]][:, :, cmass[2]] == 0, mask_imgs[labels[aa]][:, :, cmass[2]])
+            ax = fig.add_subplot(gs[aa, 2])
+            ax.imshow(bground[:, :, cmass[2]].T, cmap='Greys_r', origin='lower', interpolation='none')
+            ax.imshow(mask_roi.T, origin='lower', interpolation='none', alpha=0.8, cmap=colors.ListedColormap([colours[aa]]))
+            ax.set(yticklabels=[], xticklabels=[])  # remove the tick labels
+            ax.tick_params(left=False, bottom=False)
+        pdf.savefig(fig)
     return pdf
 
 
@@ -259,7 +291,7 @@ def make_two_violins(ax, receptors, group, pcts, ds, ds_ci, kss):
     columns = np.unique(receptors.subunit)
     # Plot data
     sb.violinplot(data=receptors, x='subunit', y='values', hue='region', inner="box",
-                  ax=ax, linewidth=0.1, grid_linewidth=1)
+                  ax=ax, linewidth=0.1, grid_linewidth=1, palette=["#0086a8", "#0086a8"])
     ax.set_title(group, fontsize=6)
     ax.tick_params(axis='y', which='both', labelsize=4, width=0.5)
     ax.set_xticklabels([])
@@ -405,6 +437,7 @@ def region_radar(receptor_median, labels, receptor_list, pdf):
     PDFPages object
 
     """
+    colours = ["#0086a8", "#0086a8"]
     fig = plt.figure()
     gs = gridspec.GridSpec(1, 2)
     # One radar for transmitters (GABA+Glu) and one for neuromodulators
@@ -428,7 +461,7 @@ def region_radar(receptor_median, labels, receptor_list, pdf):
             # Add to plot
             ax.set_xticks(angles[:-1], subunits)
             ax.set_rlabel_position(0)
-            ax.plot(angles, values, linewidth=1, linestyle='solid', label=region)
+            ax.plot(angles, values, linewidth=1, linestyle='solid', label=region, c=colours[rr])
             ax.tick_params(axis='x', labelsize=8)
             # Add legend
             # ax.legend(loc='upper left', bbox_to_anchor=(-0.4, 1), fontsize=7)
